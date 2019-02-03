0258301b3932a70eb1f2c2a6f15572721dd8e6a6
Sukanta Purkayastha <30552975+Knightrider7660@users.noreply.github.com>
2019-02-04 01:02:32 +0600

    client section

--- assets/js/active.js
+++ assets/js/active.js
@@ -19,11 +19,7 @@ $(document).ready(function(){
 	var owl = $('.owl-carousel');
     owl.owlCarousel({
         loop: true,
-        items: 1,
-        thumbs: true,
-        thumbImage: true,
-        thumbContainerClass: 'owl-thumbs',
-        thumbItemClass: 'owl-thumb-item'
+        items: 1
     });  
 
 	
